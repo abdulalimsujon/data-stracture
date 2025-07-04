1de5c0cf4613ab1a429c8ce0135c138670c3e17a
abdulAlim <alimsujon12@gmail.com>
2025-07-04 20:39:36 +0600

    code fixed

---
+++ Replace_Word.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string s;
+    string s1;
+    cin >> s >> s1;
+
+    string assign;
+    
+    for (string::iterator it = s.begin(); it != s.end(); ++it) {
+          for (string::iterator it = s1.begin(); it1 != s1.end(); ++it1) {
+       
+    }
+    }
+
+
+}
