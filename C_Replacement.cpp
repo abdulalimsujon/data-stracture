17eff447652e7e36c1fa3447735c129c6077f3ba
abdul <alimsujon12@gmail.com>
2025-07-13 14:52:43 +0600

    freq array and counting element solve

---
+++ C_Replacement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;                
+    int A[1000];             
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];         
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] > 0)
+            A[i] = 1;
+        else if (A[i] < 0)
+            A[i] = 2;
+    
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
---
+++ D_Counting_Elements.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(auto it = v.start,v.end){
+        replace(v.start,v.end,*it)
+    }
+}
