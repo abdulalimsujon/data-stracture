9f3c8b7b46ba08468d7fb31ffe1d50f5b7077a09
abdul alim <alimsujon12@gmail.com>
2025-07-16 01:23:41 +0600

    n times prblem

---
+++ H_N_Times.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+    while(n--){
+        int t;
+        char C;
+        cin >> t >> C;
+        cout  << endl;
+        for(int i=0;i<t;i++){
+            cout << C << " ";
+        }
+
+    }
+
+}
