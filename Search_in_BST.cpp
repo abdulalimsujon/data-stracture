be302a359376f20cd0700f43e544692a7145c040
abdulalimsujon <alimsujon12@gmail.com>
2025-08-03 15:07:20 +0600

    Binary search tree

---
+++ Search_in_BST.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+      int val;
+      Node *left ;
+      Node *right;
+
+      Node(int val){
+        this->val = val;
+        this->left= NULL;
+        this->right =NULL;
+      }
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node* root ;
+    if(val== -1) root = NULL;
+    else root = new Node(val);
+    queue<Node*> q;
+    if(root) q.push(root);
+
+    while(!q.empty()){
+        Node* p = q.front();
+        q.pop();
+        Node* leftNode ,*rightNode;
+        int l,r;
+        cin >> l >> r;
+        if(l==-1) leftNode= NULL;
+        else leftNode = new Node(l);
+
+        if(r==-1) rightNode = NULL;
+        else rightNode = new Node(r);
+
+        p->left = leftNode;
+        p->right = rightNode;
+
+        if(p->left) q.push(leftNode);
+        if(p->right) q.push(rightNode);
+    }
+
+    return root;
+}
+
+//search function
+
+bool search(Node* root,int v){
+
+    if(root == NULL) return false;
+    if(root->val == v) return true;
+
+    if(root->val > v){
+       return   search(root->left,v);
+       
+    }else{
+     return   search(root->right,v);
+       
+    }
+
+}
+int main(){
+
+    Node* root = input_tree();
+
+    int val;
+
+    cin >> val;
+
+    if(search(root,val)){
+        cout << "Found" << endl;
+    }else{
+        cout << "Not found" << endl;
+    }
+
+}
